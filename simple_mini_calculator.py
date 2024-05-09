ad11f987f7f7e2e6e9671f7357e8b4c83f68618c
ProgrammerDaryl <ProgrammerDaryl>
2024-05-10 03:36:23 +0800

    Added: Greetings, Inputs, and Two Operators

--- simple_mini_calculator.py
+++ simple_mini_calculator.py
@@ -2,3 +2,18 @@
 #ask the user to input the first number
 #ask the user to input the second number
 #ask the user which operation to use
+def mini_calculator():
+    print("Hello User!")
+    number_1 = float(input("Please enter your first number here: "))
+    number_2 = float(input("Please enter your second number here: "))
+    operation = input("Please select an operator: +-*/  ")
+
+    if operation == "+":
+        answer = number_1 + number_2
+    
+    elif operation == "-":
+        answer = number_1 - number_2
+        
+
+
+
